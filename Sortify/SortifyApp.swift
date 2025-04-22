ab9c3554461227cd41d637e7416d5d0539acf779
Gourav Kumar <gourav.kumar@saavn.com>
2025-04-22 17:27:03 +0530

    Initial Commit

---
+++ Sortify/SortifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortifyApp.swift
+//  Sortify
+//
+//  Created by Gourav Kumar on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SortifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SortifyTests/SortifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  SortifyTests.swift
+//  SortifyTests
+//
+//  Created by Gourav Kumar on 22/04/25.
+//
+
+import Testing
+@testable import Sortify
+
+struct SortifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
