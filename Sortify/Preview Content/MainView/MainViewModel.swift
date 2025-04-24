aa6372f944d6ca633f67758ea90680e68e3d6aab
Adarsh Singh <asingh11@ee.iitr.ac.in>
2025-04-24 20:15:59 +0530

    final

--- Sortify/Preview Content/MainView/MainViewModel.swift
+++ Sortify/Preview Content/MainView/MainViewModel.swift
@@ -8,11 +8,16 @@
 import Foundation
 import SwiftUICore
 
+// MARK: - Graph Data
+
 class GraphData: ObservableObject {
     @Published var startIndex: Int = 0
     @Published var endIndex: Int = 0
-    @Published var array : [Int] = []
+    @Published var array: [Int] = []
 }
+
+// MARK: - Main ViewModel
+
 class MainViewModel: ObservableObject {
     @Published var algo: Algorithms = .bubble
     @Published var time: Int = 0
@@ -20,155 +25,134 @@ class MainViewModel: ObservableObject {
     @Published var isShowSetting: Bool = false
     @Published var settingData = SettingsViewModel()
     @Published var graphData = GraphData()
-    private var timer : Timer?
-    
+
+    private var timer: Timer?
+
+    // MARK: - Init
+
     init() {
         generateRandomArray()
     }
-    
-    func startSorting() {
-        isRunning = true
-        startTimer()
-        Task{
-            switch algo {
-            case .bubble:
-                await bubbleSort()
-            case .selection:
-                await selectionSort()
-            case .insertion:
-                await insertionSort()
-            case .merge:
-                await mergeSort()
-            }
-            
-        }
-        
-    }
+
+    // MARK: - Timer Control
+
     func startTimer() {
         timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
-        
     }
+
     @objc func updateTime() {
         time += 10
     }
-    
-    func reset() {
-        stopSorting()
-        generateRandomArray()
-        time = 0
+
+    // MARK: - Control Actions
+
+    func startSorting() {
+        isRunning = true
+        startTimer()
+        Task {
+            switch algo {
+            case .bubble: await bubbleSort()
+            case .selection: await selectionSort()
+            case .insertion: await insertionSort()
+            case .merge: await mergeSort()
+            }
+        }
     }
+
     func stopSorting() {
         isRunning = false
         timer?.invalidate()
         timer = nil
     }
+
+    func reset() {
+        stopSorting()
+        generateRandomArray()
+        time = 0
+    }
+
     func generateRandomArray() {
         let size = Int(settingData.arrSize)
-        let randomArray = (0..<size).map { _ in Int.random(in: 1...100) }
-        graphData.array = randomArray
-        graphData.endIndex = 0
+        graphData.array = (0..<size).map { _ in Int.random(in: 1...100) }
         graphData.startIndex = 0
+        graphData.endIndex = 0
     }
+
+    // MARK: - Sorting Algorithms
+
+    // MARK: Bubble Sort
     func bubbleSort() async {
-        for (index,_) in graphData.array.enumerated() {
-            await MainActor.run { [weak self] in
-                self?.graphData.startIndex = index }
-            
-            for i in 0..<graphData.array.count-1 {
-                if !isRunning {
-                    return
-                }
-                if graphData.array[i] > graphData.array[i+1] {
-                    await MainActor.run { [weak self] in
-                        self?.graphData.array.swapAt(i, i+1)
-                        self?.graphData.endIndex = i+1
+        for index in 0..<graphData.array.count {
+            await MainActor.run { self.graphData.startIndex = index }
+            for i in 0..<graphData.array.count - 1 {
+                guard isRunning else { return }
+                if graphData.array[i] > graphData.array[i + 1] {
+                    await MainActor.run {
+                        self.graphData.array.swapAt(i, i + 1)
+                        self.graphData.endIndex = i + 1
                     }
-                    try? await Task.sleep(nanoseconds: UInt64(Int(settingData.duration*100000000)))
+                    try? await Task.sleep(nanoseconds: delay)
                 }
             }
         }
-        await MainActor.run { [weak self] in
-            self?.stopSorting()
-        }
+        await MainActor.run { stopSorting() }
     }
+
+    // MARK: Insertion Sort
     func insertionSort() async {
-        let n = graphData.array.count
-        for i in 1..<n {
+        for i in 1..<graphData.array.count {
             let key = graphData.array[i]
             var j = i - 1
-            
-            await MainActor.run { [weak self] in
-                self?.graphData.startIndex = i
-            }
-            
+            await MainActor.run { self.graphData.startIndex = i }
             while j >= 0 && graphData.array[j] > key {
-                if !isRunning {
-                    return
+                guard isRunning else { return }
+                await MainActor.run {
+                    self.graphData.array[j + 1] = self.graphData.array[j]
+                    self.graphData.endIndex = j + 1
                 }
-                
-                await MainActor.run { [weak self] in
-                    self?.graphData.array[j + 1] = self?.graphData.array[j] ?? 0
-                    self?.graphData.endIndex = j + 1
-                }
-                
-                try? await Task.sleep(nanoseconds: UInt64(Int(settingData.duration * 1_000_000_00)))
+                try? await Task.sleep(nanoseconds: delay)
                 j -= 1
             }
-            
-            await MainActor.run { [weak self] in
-                self?.graphData.array[j + 1] = key
-                self?.graphData.endIndex = j + 1
+            await MainActor.run {
+                self.graphData.array[j + 1] = key
+                self.graphData.endIndex = j + 1
             }
-            
-            try? await Task.sleep(nanoseconds: UInt64(Int(settingData.duration * 1_000_000_00)))
+            try? await Task.sleep(nanoseconds: delay)
         }
-        
-        await MainActor.run { [weak self] in
-            self?.stopSorting()
-        }
+        await MainActor.run { stopSorting() }
     }
+
+    // MARK: Selection Sort
     func selectionSort() async {
         let n = graphData.array.count
         for i in 0..<n {
             var minIndex = i
-            
-            await MainActor.run { [weak self] in
-                self?.graphData.startIndex = i
-            }
-            
+            await MainActor.run { self.graphData.startIndex = i }
             for j in i+1..<n {
-                if !isRunning { return }
+                guard isRunning else { return }
                 if graphData.array[j] < graphData.array[minIndex] {
                     minIndex = j
                 }
-                await MainActor.run { [weak self] in
-                    self?.graphData.endIndex = j
-                }
-                try? await Task.sleep(nanoseconds: UInt64(Int(settingData.duration * 1_000_000_00)))
+                await MainActor.run { self.graphData.endIndex = j }
+                try? await Task.sleep(nanoseconds: delay)
             }
-            
             if i != minIndex {
-                await MainActor.run { [weak self] in
-                    self?.graphData.array.swapAt(i, minIndex)
-                    self?.graphData.endIndex = minIndex
+                await MainActor.run {
+                    self.graphData.array.swapAt(i, minIndex)
+                    self.graphData.endIndex = minIndex
                 }
-                try? await Task.sleep(nanoseconds: UInt64(Int(settingData.duration * 1_000_000_00)))
+                try? await Task.sleep(nanoseconds: delay)
             }
         }
-        
-        await MainActor.run { [weak self] in
-            self?.stopSorting()
-        }
+        await MainActor.run { stopSorting() }
     }
-    
-    
+
+    // MARK: Merge Sort
     func mergeSort() async {
         await mergeSortHelper(0, graphData.array.count - 1)
-
-        await MainActor.run { [weak self] in
-            self?.stopSorting()
-        }
+        await MainActor.run { stopSorting() }
     }
+
     private func mergeSortHelper(_ left: Int, _ right: Int) async {
         if left < right {
             let mid = (left + right) / 2
@@ -178,9 +162,8 @@ class MainViewModel: ObservableObject {
         }
     }
 
-
-    func merge(_ left: Int, _ mid: Int, _ right: Int) async {
-        if !isRunning { return }
+    private func merge(_ left: Int, _ mid: Int, _ right: Int) async {
+        guard isRunning else { return }
 
         let leftArray = Array(graphData.array[left...mid])
         let rightArray = Array(graphData.array[mid+1...right])
@@ -188,58 +171,47 @@ class MainViewModel: ObservableObject {
         var i = 0, j = 0, k = left
 
         while i < leftArray.count && j < rightArray.count {
-            if !isRunning { return }
-
-            await MainActor.run { [weak self] in
-                self?.graphData.startIndex = k
-                self?.graphData.endIndex = k
+            guard isRunning else { return }
+            await MainActor.run {
+                self.graphData.startIndex = k
+                self.graphData.endIndex = k
+                self.graphData.array[k] = (leftArray[i] <= rightArray[j]) ? leftArray[i] : rightArray[j]
             }
-
-            if leftArray[i] <= rightArray[j] {
-                await MainActor.run { [weak self] in
-                    self?.graphData.array[k] = leftArray[i]
-                }
-                i += 1
-            } else {
-                await MainActor.run { [weak self] in
-                    self?.graphData.array[k] = rightArray[j]
-                }
-                j += 1
-            }
-
-            try? await Task.sleep(nanoseconds: UInt64(Int(settingData.duration * 1_000_000_00)))
+            if leftArray[i] <= rightArray[j] { i += 1 } else { j += 1 }
+            try? await Task.sleep(nanoseconds: delay)
             k += 1
         }
 
         while i < leftArray.count {
-            if !isRunning { return }
-            await MainActor.run { [weak self] in
-                self?.graphData.startIndex = k
-                self?.graphData.endIndex = k
-                self?.graphData.array[k] = leftArray[i]
+            guard isRunning else { return }
+            await MainActor.run {
+                self.graphData.array[k] = leftArray[i]
+                self.graphData.startIndex = k
+                self.graphData.endIndex = k
             }
-            try? await Task.sleep(nanoseconds: UInt64(Int(settingData.duration * 1_000_000_00)))
+            try? await Task.sleep(nanoseconds: delay)
             i += 1
             k += 1
         }
 
         while j < rightArray.count {
-            if !isRunning { return }
-            await MainActor.run { [weak self] in
-                self?.graphData.startIndex = k
-                self?.graphData.endIndex = k
-                self?.graphData.array[k] = rightArray[j]
+            guard isRunning else { return }
+            await MainActor.run {
+                self.graphData.array[k] = rightArray[j]
+                self.graphData.startIndex = k
+                self.graphData.endIndex = k
             }
-            try? await Task.sleep(nanoseconds: UInt64(Int(settingData.duration * 1_000_000_00)))
+            try? await Task.sleep(nanoseconds: delay)
             j += 1
             k += 1
         }
     }
 
+    // MARK: - Helpers
 
-
-    
-    
-    
+    private var delay: UInt64 {
+        UInt64(Int(settingData.duration * 100_000_000)) // ms to ns
+    }
 }
+
 
